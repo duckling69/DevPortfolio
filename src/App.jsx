84c43fb26fc8f08778bd7ab526385eb374ce4a27
Vivek jha <85884487+duckling69@users.noreply.github.com>
2022-05-27 03:08:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect,useState } from 'react'
+import Intro from './components/Intro'
+import Skills from './components/Skills';
+import './styles/App.css'
+function App() {
+  const [theme, setTheme] = useState(null);
+
+	useEffect(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	const handleThemeSwitch = () => {
+		setTheme(theme === 'dark' ? 'light' : 'dark');
+	};
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+  return (
+    <>
+      <button
+				type="button"
+				onClick={handleThemeSwitch}
+				className="fixed z-10 right-2 top-2 bg-indigo-500 text-lg p-1 rounded-md"
+			>
+				{theme === 'dark' ? '🌙' : '🌞'}
+			</button>
+      <div className='font-mono bg-white dark:bg-slate-900 trans'>
+        <Intro/>
+        <Skills/>
+      </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/Intro.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles/styles.css';
+function Intro() {
+  return (
+    <div className='flex items-center justify-center flex-col py-20'>
+        <div className='text-center'>
+            <h1 className='trans text-2xl md:text-4xl mb-1 md:mb-3 text-indigo-600 font-semibold dark:text-indigo-500'>Hi, This is <br className="block md:hidden" />
+  <span className="relative">
+    <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap text-brand-accent">
+    Vivek !!
+    </span>
+    <span
+      className="trans {`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-white dark:bg-slate-900 w-full animate-type will-change`}"
+    ></span>
+  </span></h1>
+            <p className='trans text-md md:text-xl max-w-md mb-3 text-gray-600 dark:text-gray-300'>Fresher at NIT Rourkela, DEV and Programming enthusiast.</p>
+            <div className='flex flex-row justify-center'>
+              <a href='#' className='inline-block px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-white hover:text-indigo-600 hover:border-indigo-600 md:text-md'>Resume</a>
+              <a href='#' className='inline-block px-8 mx-2 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 border-indigo-600 bg-white hover:bg-indigo-600 hover:text-white hover:border-indigo-600 md:text-md'>Lets talk</a>
+            </div>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Intro
